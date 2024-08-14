f965b93ef9dd23803db9a84212ecdd3cf1481a4c
AI-Voodoo <jsims@mongol.ca>
2024-08-14 17:32:01 -0400

    Your commit message here

--- data_processing/data_loaders.py
+++ data_processing/data_loaders.py
@@ -10,15 +10,15 @@ from nlp.embeddings import EmbeddingModel
 
 class LoadData:
     def __init__(self) -> None:
-        self.spacy_work = SpacyNLP()
         self.target_entity_list = ["PERSON", "NORP", "FAC", "ORG", "GPE", "LOC", "PRODUCT", "EVENT", "WORK_OF_ART", "LAW", "LANGUAGE", "MONEY"]
+
+        self.spacy_work = SpacyNLP()
         self.assess_emails = LlmAssesText()
         self.target_prompts = TargetStrings()
         self.embedding_work = EmbeddingModel()
 
         self.law_target = self.target_prompts.collection_goals_legal()
         self.money_target = self.target_prompts.collection_goals_money()
-        #self.it_target = self.target_prompts.collection_goals_it()
         self.exlclusion_target = self.target_prompts.exlclusion_goals()
 
     def delete_file(self, file_path) -> None:
@@ -41,9 +41,14 @@ class LoadData:
     def clean_email_content(self, content: str) -> str:
         if "X-FileName:" in content:
             clean_content = content.split("X-FileName:", 1)[-1]
-            return clean_content.replace("=01,", "'").replace("=20", "").replace("= ", "").replace("=09", "").str.replace(r'\s+', ' ', regex=True)
+            clean_content = clean_content.replace("=01,", "'")
+            clean_content = clean_content.replace("=20", "")
+            clean_content = clean_content.replace("= ", "")
+            clean_content = clean_content.replace("=09", "")
+            clean_content = re.sub(r'\s+', ' ', clean_content)
+            return clean_content
         return content
-    
+        
     def load_csv_to_df(self, csv_path) -> pd.DataFrame:
         df = pd.read_csv(csv_path, header=None, encoding='utf-8')
         if df.shape[1] < 2:
@@ -58,18 +63,18 @@ class LoadData:
         emails = []
         for _, row in df.iterrows():
             print(f"Analyzing email from: {row[0]}")
-            email_content = row[1]  # This is the email content
+
+            email_content = row[1]
             clean_email_content = self.clean_email_content(email_content)
             email_senders, email_recipients = self.recover_email(email_content)
+            if not len(email_recipients) >= 1:
+                continue
             entities = self.spacy_work.get_entities_by_type(email_content, self.target_entity_list)
-
             law_entities, money_entities = self.spacy_work.process_law_money_entities(entities)
-            #value_bool = self.assess_emails.llm_assess_value(clean_email_content)
             if not (len(law_entities) >= 1 or len(money_entities) >=1 ):
                 continue
             law_sentences, money_sentences = self.spacy_work.extract_sentences_with_entities(clean_email_content, law_entities, money_entities)
-            
-            # Initialize scores
+
             law_score = 0.00
             money_score = 0.00
 
@@ -80,7 +85,7 @@ class LoadData:
             exclusion_score = self.embedding_work.compare_text_for_similarity(self.exlclusion_target, clean_email_content)
             if law_score <= 0.29 and money_score <= 0.29:
                 continue
-            # Prepare email data
+
             email_data = {
                 "email_meta_data": row[0],  # The metadata (first column)
                 "clean_content": clean_email_content,
